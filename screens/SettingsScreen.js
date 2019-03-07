37c18e8b74d8a269a516ea4d7489462c3c20c1d4
Lakshay Tyagi <lakshaytyagi0001@gmail.com>
2019-03-08 00:06:05 +0530

    4th

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View,Text,Button} from 'react-native';
+import * as firebase from 'firebase';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
     title: 'Settings',
   };
 
+  onSignoutPress = () => {
+    firebase.auth().signOut();
+  }
+
   render() {
     return (
       <View>
       <Text>Setting Screen</Text>
       <Text>Coming Soon</Text>
+      < Button title='Signout' onPress={this.onSignoutPress}/>
+
       </View>
     );
   }
